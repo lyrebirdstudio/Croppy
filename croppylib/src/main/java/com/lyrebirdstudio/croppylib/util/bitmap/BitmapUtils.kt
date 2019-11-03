b51566c890ded446cc862f0c89e0f3b2043fd035
Mohammad Sianaki <m.rsianaki@yahoo.com>
2019-11-04 01:01:56 +0330

    Change bitmap operation classes to singleton

--- croppylib/src/main/java/com/lyrebirdstudio/croppylib/util/bitmap/BitmapUtils.kt
+++ croppylib/src/main/java/com/lyrebirdstudio/croppylib/util/bitmap/BitmapUtils.kt
@@ -1,13 +1,35 @@
 package com.lyrebirdstudio.croppylib.util.bitmap
 
 import android.content.Context
+import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.net.Uri
+import com.lyrebirdstudio.croppylib.ui.CroppedBitmapData
+import io.reactivex.Completable
 import io.reactivex.Single
+import java.io.File
+import java.io.FileOutputStream
 
-class BitmapResizer(private val context: Context) {
+object BitmapUtils {
 
-    fun resize(uri: Uri): Single<ResizedBitmap> {
+    private const val MAX_SIZE = 1024
+
+
+    fun saveBitmap(croppedBitmapData: CroppedBitmapData, file: File): Completable {
+        return Completable.create {
+            try {
+                FileOutputStream(file).use { out ->
+                    croppedBitmapData.croppedBitmap?.compress(Bitmap.CompressFormat.PNG, 100, out)
+                    it.onComplete()
+                }
+            } catch (e: Exception) {
+                it.onError(e)
+            }
+
+        }
+    }
+
+    fun resize(uri: Uri, context: Context): Single<ResizedBitmap> {
         return Single.create {
             val options = BitmapFactory.Options()
             options.inJustDecodeBounds = true
@@ -36,9 +58,5 @@ class BitmapResizer(private val context: Context) {
 
             it.onSuccess(ResizedBitmap(resizedBitmap))
         }
-    }
-
-    companion object {
-        private const val MAX_SIZE = 1024
     }
 }
